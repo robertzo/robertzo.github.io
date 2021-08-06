8f892cee2adf8b813fc6733bfa61a1d4134d525b
Robert Zou <robertzou1234@gmail.com>
2021-08-06 15:30:17 -0600

    Changes

---
+++ robertzou.com/wp-content/litespeed/cssjs/370212b72.js
@@ -0,0 +1,2 @@
+var ktjarforEach=function(array,callback,scope){for(var i=0;i<array.length;i++){callback.call(scope,i,array[i]);}};var myNodeList=document.querySelectorAll('.kt-jarallax');ktjarforEach(myNodeList,function(index,value){jarallax(value,{speed:0.5,elementInViewport:value,});});
+;
